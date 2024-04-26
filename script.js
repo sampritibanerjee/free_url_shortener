4824f51bc132da51e8d4f88adf5adf95fcb732df
Sampriti Banerjee <88034572+sampritibanerjee@users.noreply.github.com>
2024-04-26 22:00:11 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,17 +2,30 @@ let btn = document.getElementById("shorten");
 
 btn.addEventListener('click', short);
 
+// not working 
+// async function short() { 
+//     let longURL = document.getElementById("longurl").value;
+//     let shortURL = document.getElementById("shorturl");
+
+//     const result = await fetch(`https://api.shrtco.de/v2/shorten?url=${longURL}`);
+//     const data = await result.json();
+
+//     shortURL.value = data.result.short_link;
+// }
 
 async function short() {
     let longURL = document.getElementById("longurl").value;
     let shortURL = document.getElementById("shorturl");
 
-    const result = await fetch(`https://api.shrtco.de/v2/shorten?url=${longURL}`);
-    const data = await result.json();
+    const response = await fetch(`https://tinyurl.com/api-create.php?url=${encodeURIComponent(longURL)}`);
 
-    shortURL.value = data.result.short_link;
+    if (response.ok) {
+        const data = await response.text();
+        shortURL.value = data;
+    } else {
+        console.error('Failed to shorten URL:', response.statusText);
+    }
 }
-
 
 let newURL = document.getElementById("shorturl");
 let coptButton = document.getElementById("copy");
